26508c2d1232361db0d642996636bff963d33cdd
Agnisha <2025.cse09.agnishade@gmail.com>
2023-11-14 19:33:29 +0530

    Added features to add and delete notes with title and content

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,14 +1,85 @@
-import React from "react";
+// import React, { useState } from "react";
+// import Header from "./header";
+// import Footer from "./footer";
+// import Note from "./note";
+// import CreateArea from "./CreateArea";
+
+// function App() {
+
+//   const [notes, setNotes] = useState([]);
+
+//   function addNote(newNote) {
+//     setNotes(prevNotes => {
+//       return [...prevNotes, newNote];
+//     });
+//   }
+//   function deleteNote(id) {
+//     setNotes(prevNotes => {
+//       return prevNotes.filter((noteItem, index) => {
+//         return index !== id;
+//       });
+//     });
+//   }
+//   return (
+//     <div>
+//       <Header />
+//       <CreateArea onAdd={addNote}/>
+//       {notes.map((noteItem, index) => {
+//         return (
+//           <Note
+//             key={index}
+//             id={index}
+//             title={noteItem.title}
+//             content={noteItem.content}
+//             onDelete={deleteNote}
+//           />
+//         );
+//       })}      
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from "react";
 import Header from "./header";
 import Footer from "./footer";
 import Note from "./note";
-
+import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      <Note />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
